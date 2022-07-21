7f64963f4ddf2823cf23650c689e172c7e7e9205
Sean CH Wu <sean.ch.wu@ibm.com>
2022-07-21 11:39:14 +0800

    create react-app map

---
+++ main/process/MapProcess.py
@@ -0,0 +1,20 @@
+class TraceabilityMA:
+    def object(self):
+        obj = ['Maintain traceability products',
+               'Provide technical assistance and advice based on expertise'
+               ]
+        return obj
+
+    def challenge(self):
+        challenge = ['Quickly understand traceability solution',
+            'industry know how ',
+            'related skills '
+               ]
+        return challenge
+
+
+if __name__ == '__main__':
+    traceabilityMa = TraceabilityMA()
+    traceabilityMa.process()
+
+
