24f8cc98120fce12ff0741ed5b1f68022f85138e
Sean CH Wu <sean.ch.wu@ibm.com>
2022-08-10 15:57:15 +0800

    zipfile chinese decode garbled problem solve

--- main/process/UnzipFileProcess.py
+++ main/process/UnzipFileProcess.py
@@ -3,6 +3,8 @@ import os
 import zipfile
 
 import yaml
+from pathlib import Path
+from utils.Logger import Logger
 
 
 class UnzipFileProcess:
@@ -19,19 +21,26 @@ class UnzipFileProcess:
     def zip_file(self):
         return 'zip file done'
 
-    def unzip_file(self, path_to_zip_file: str, directory_to_extract_to: str):
+    def unzip_file(self, path_to_zip_file: str, directory_to_extract_to: str, zip_name:str):
         with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-            zip_ref.extractall(directory_to_extract_to)
+            for file_name in zip_ref.namelist():
+                csv_name = zip_name.replace('zip', 'csv')
+                extracted_path = Path(zip_ref.extract(file_name))
+                # zipfile chinese decode problem, use zip file name replace
+                extracted_path.rename(directory_to_extract_to + csv_name)
 
     def process(self):
         directory = os.fsencode(self.zip_path)
 
         for file in os.listdir(directory):
             filename = os.fsdecode(file)
+            file_path = self.zip_path + filename
+
             if filename.endswith(".zip"):
-                file_path = self.zip_path + filename
-                self.unzip_file(file_path, self.csv_path)
+                self.unzip_file(file_path, self.csv_path, filename)
+                Logger.info('unzip file: ' + file_path)
             else:
+                Logger.info('this file type is not zip: ' + file_path)
                 continue
 
 
